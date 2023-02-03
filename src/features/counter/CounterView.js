ef3d05836ae968fbc41988f3797c0b6e9ef3cfd5
AF Tamim <itamim345@gmail.com>
2023-02-03 20:58:21 +0530

    Done with redux counter

---
+++ src/features/counter/CounterView.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment, reset } from './counterSlice';
+
+export default function CounterView() {
+    const count = useSelector((state) => state.counter.count);
+    const dispatch = useDispatch();
+  return (
+    <div>
+      <h2>Counter Value: {count}</h2>
+      <button onClick={()=> dispatch(increment())}>Increase</button>
+      <button onClick={() => dispatch(decrement())}>Decrease</button>
+      <button onClick={() => dispatch(reset())}>Reset</button>
+    </div>
+  );
+}
